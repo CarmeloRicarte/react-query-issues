a97283b5a015b774608dc52e11f061427b1dd67a
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2023-01-13 17:37:53 +0100

    feat: set placeholderData for show data while fetch is resolving

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -12,6 +12,27 @@ const getLabels = async (): Promise<ILabel[]> => {
 export const useLabels = () => {
   const labelsQuery = useQuery(["labels"], getLabels, {
     staleTime: 1000 * 60 * 60, // data update every hour
+    // initialData combined with staleTime, trusts in initialData by time setted in staleTime
+
+    placeholderData: [
+      // data to show before fetching is resolved
+      {
+        id: 791921801,
+        node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
+        url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
+        name: "❤️",
+        color: "ffffff",
+        default: false,
+      },
+      {
+        id: 717031390,
+        node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+        name: "good first issue",
+        color: "6ce26a",
+        default: true,
+      },
+    ],
   });
   return labelsQuery;
 };
