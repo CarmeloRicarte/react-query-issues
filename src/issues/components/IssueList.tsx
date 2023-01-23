8e6764e34c35516fab66f93a7ff0d688e3bc4d06
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2023-01-23 17:07:59 +0100

    feat: save in state the active filter passing state and setter from ListView to IssueList

--- src/issues/components/IssueList.tsx
+++ src/issues/components/IssueList.tsx
@@ -1,24 +1,45 @@
 import { FC } from "react";
-import { IIssue } from "../interfaces";
+import { IIssue, State } from "../interfaces";
 import { IssueItem } from "./IssueItem";
 
 interface IssueListProps {
   issues: IIssue[];
+  state?: State;
+  onStateChanged: (state?: State) => void;
 }
 
-export const IssueList: FC<IssueListProps> = ({ issues }) => {
+export const IssueList: FC<IssueListProps> = ({
+  issues,
+  state,
+  onStateChanged,
+}) => {
   return (
     <div className="card border-white">
       <div className="card-header bg-dark">
         <ul className="nav nav-pills card-header-pills">
           <li className="nav-item">
-            <a className="nav-link active">All</a>
+            <a
+              className={`nav-link ${!state ? "active" : ""}`}
+              onClick={() => onStateChanged()}
+            >
+              All
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link">Open</a>
+            <a
+              className={`nav-link ${state === State.Open ? "active" : ""}`}
+              onClick={() => onStateChanged(State.Open)}
+            >
+              {State.Open}
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link">Closed</a>
+            <a
+              className={`nav-link ${state === State.Closed ? "active" : ""}`}
+              onClick={() => onStateChanged(State.Closed)}
+            >
+              {State.Closed}
+            </a>
           </li>
         </ul>
       </div>
