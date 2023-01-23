936cd361e2dd6c11a9d95be508223b781302088c
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2023-01-23 17:56:36 +0100

    feat(GitApp.tsx): update title

--- src/GitApp.tsx
+++ src/GitApp.tsx
@@ -5,7 +5,8 @@ export const GitApp: FC = () => {
   return (
     <div className="container mt-3">
       <h1>
-        Git Issues <small>Seguimiento de problemas</small>{" "}
+        React Git Issues{" "}
+        <small>Seguimiento de problemas en el repositorio de React</small>{" "}
       </h1>
       <Outlet />
     </div>
